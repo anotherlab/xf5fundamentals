dd4f572a825deafec0c2a730e1ce23e1d043e71e
Chris Miller <chris@rajapet.net>
2022-01-17 11:40:51 -0500

    Added Dailyforecast SQLite demo

---
+++ DailyForecast/sqlite/DailyForecast/DailyForecast/Models/DailyRange.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyForecast.Models
+{
+	public class DailyRange
+	{
+		public DateTime ForecastDate { get; set; }
+		public double LowTemp { get; set; }
+		public double HighTemp { get; set; }
+	}
+}
